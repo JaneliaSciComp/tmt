21caa77cf3df381af1a6dfd3fae6f31506e1f790
Adam L. Taylor <adamlyletaylor@gmail.com>
2021-06-15 00:03:01 -0400

    Fixed bug in are_paired_descriptors_close_enough.m

--- mouselighty/are_paired_descriptors_close_enough.m
+++ mouselighty/are_paired_descriptors_close_enough.m
@@ -1,7 +1,13 @@
 function result = are_paired_descriptors_close_enough(pd1, pd2)
-    x1 = pd1.X ;
-    x2 = pd2.X ;
-    y1 = pd1.Y ;
-    y2 = pd2.Y ;
+    % The order of the pairs can be different between the two versions, so sort the
+    % rows to deal with this
+    [x1,ix1] = sortrows(pd1.X) ;
+    y1_raw = pd1.Y ;
+    y1 = y1_raw(ix1,:) ;
+    
+    [x2,ix2] = sortrows(pd2.X) ;
+    y2_raw = pd2.Y ;
+    y2 = y2_raw(ix2,:) ;   
+    
     result = size(x1,1)==size(x2,1) && size(y1,1)==size(y2,1) && isequal(x1,x2) && isequal(y1,y2) ;    
 end
