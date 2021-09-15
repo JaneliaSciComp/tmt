dc471134e6b380b03d170b6ecd518a65b05d00c1
Adam L. Taylor <adamlyletaylor@gmail.com>
2021-09-15 11:13:25 -0400

    Made simple_dir.m handle symlink sizes better

--- mouselighty/simple_dir.m
+++ mouselighty/simple_dir.m
@@ -2,7 +2,9 @@ function [result, is_folder, file_size, datenum] = simple_dir(template)
     s = dir(template) ;
     raw_result = {s.name} ;
     raw_is_folder = [s.isdir] ;
-    raw_file_size = [s.bytes] ;
+    raw_file_size = cellfun(@(x)(fif(isempty(x),nan,x)), {s.bytes}) ;
+        % You'd think raw_file_size = [s.bytes] would work, but for symlinks s(i).bytes is [],
+        % so we want to replace those with some scalar double.
     raw_datenum = [s.datenum] ;
     [~,indices_of_keepers] = setdiff(raw_result, {'.' '..'}) ;
     result = raw_result(indices_of_keepers) ;
