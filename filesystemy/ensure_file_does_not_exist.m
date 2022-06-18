a84a85236f70550ae12fc8996fd970b1dbf10d93
Adam L. Taylor <adamlyletaylor@gmail.com>
2022-06-18 01:30:08 -0400

    Couple of minor improvements

--- filesystemy/ensure_file_does_not_exist.m
+++ filesystemy/ensure_file_does_not_exist.m
@@ -1,6 +1,15 @@
 function ensure_file_does_not_exist(raw_file_path)
+    if isempty(raw_file_path) ,
+        error('File path cannot be empty') ;
+    end
     file_path = absolute_filename(raw_file_path) ;
-    if exist(file_path, 'file') ,
+    if isempty(file_path) ,
+        error('File path cannot be empty') ;
+    elseif strcmp(file_path, '/') ,
+        error('Not going to rm -rf /, sorry') ;        
+    elseif exist(file_path, 'file') ,
         system_from_list_with_error_handling({'rm', '-rf', file_path}) ;
+    else
+        % do nothing, all is well
     end
 end
